52d89733690d0253caa8aa63e408b1fc227fdf1e
sencangit <senthilkar14@gmail.com>
2025-04-13 17:56:17 -0400

    Removing unwanted comments

--- src/main/java/com/finance/marketdata/restservices/util/SampleEncoder.java
+++ src/main/java/com/finance/marketdata/restservices/util/SampleEncoder.java
@@ -1,14 +1,26 @@
 package com.finance.marketdata.restservices.util;
 
+import java.util.Base64;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SampleEncoder {
 
 	public static void main(String[] args) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode("root");
+        String encodedPassword = passwordEncoder.encode("testpass");
          
         System.out.println(encodedPassword);
+        
+        String originalInput = "sql_Root";
+        String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        
+        System.out.println(encodedString);
+        
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        
+        System.out.println(decodedString);
 
 	}
 
